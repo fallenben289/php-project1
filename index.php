b31bcec5b3b67d044e53d5056009f6cc8732d819
fallenben289 <65033733+fallenben289@users.noreply.github.com>
2022-09-26 09:32:13 +0800

    Update PHP Project.

--- index.php
+++ index.php
@@ -11,6 +11,8 @@
 
 <?php
   echo "<h2>SIMPLE CMS</h2>";
+
+  //Config File is required to be included
     require "config.php";
 ?>
 
@@ -18,8 +20,8 @@
 <body>
     <nav>
         <ul>
-            <li><a href="#">Insert</a></li>
-            <li><a href="#">View</a></li>
+            <li><a href="#Insert">Insert</a></li>
+            <li><a href="#View">View</a></li>
         </ul>
     </nav>
 
@@ -37,6 +39,44 @@
             <input type="submit" style="margin-top:0.4%" name="submit" value="Add Data">
             </form>
     </div>    
+    
+    <?php
+
+
+//To Avoid warning array key undefined
+if (isset($_POST['liabilities'])) {
+    $name= $_POST["name"];
+    $liabilities= $_POST["liabilities"];
+    $date= $_POST["date"];
+
+    //To Insert Data to MYSQL
+    $sql = "INSERT INTO personal_ledger (name, liabilities, date) VALUES ('$name','$liabilities','$date')";
+}
+
+    //Check if the Form is not empty
+    if (!isset($_POST['name']) and !isset($_POST['liabilities']) and !isset($_POST['date'])) {
+        echo "Not Inserted.";
+    } else {
+        //If Form is filled
+        if (mysqli_query($con, $sql)) {
+            //After checking the filled form insert the needed value
+            echo "New Record Inserted.";
+        }
+    }
+
+
+    //To Calculate the value of column of liabilities through the table of personal ledger
+            $res = mysqli_query($con, 'SELECT SUM(liabilities) AS sum FROM personal_ledger');
+            $row = $res->fetch_array();
+            $sum = $row['sum'];
+
+
+            
+            echo "<h2>Total<br>Liabilities=".$sum."</h2>";
+           
+        mysqli_close($con);
+
+    ?>
 
 </body>
 </html>
